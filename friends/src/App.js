0f85e2856023de5c43393653f5b116f129cc38ef
garybot <argot.pryer@gmail.com>
2019-12-09 13:57:17 -0800

    got login working.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
+import PrivateRoute from './components/PrivateRoute.js';
 import Login from './components/Login.js';
+import FriendsList from './components/FriendsList.js';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="/friends">Friends List</Link>
+        </nav>
+        <Switch>
+          <PrivateRoute exact path="/friends" component={FriendsList} />
+          <Route exact path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import {axiosWithAuth} from '../utils/axiosWithAuth.js';
 
-const Login = () => {
+const Login = (props) => {
   const [userCredentials, setUserCredentials] = useState({username: "", password: ""});
   
   const handleChanges = (evt) => {
@@ -19,12 +18,13 @@ const Login = () => {
       .post('http://localhost:5000/api/login', userCredentials)
       .then(res => {
         localStorage.setItem('token', res.data.payload);
+        props.history.push('/friends');
       })
       .catch(err => console.log(err));
   }
   
   return (
-    <form onSubmit={handleSubmit}>
+    <form className='login-form' onSubmit={handleSubmit}>
       <input type="text" name="username" id="username" placeholder="username" value={userCredentials.username} onChange={handleChanges}/>
       <input type="text" name="password" id="password" placeholder="password" value={userCredentials.password} onChange={handleChanges}/>
       <button>Login</button>
